5595b64a6049a1960f69989ea21ab3f7acc75d7a
Amir Raza <amir_mtp@yahoo.com>
2020-01-31 21:26:28 +0500

    init

---
+++ app/src/main/java/com/encodingstream/app/android/devopspipelinetest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.encodingstream.app.android.devopspipelinetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
